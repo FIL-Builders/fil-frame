302e613b025d5e7cebcd1308606f0a4a9a57616f
xBalbinus <xiangan9@gmail.com>
2024-10-22 18:43:16 -0400

    switch frontend default network to calibration

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -10,7 +10,8 @@ export type ScaffoldConfig = {
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.hardhat],
+  // Note: The network is calibration by default due to Filecoin-specific libraries being used (e.g. deal client)
+  targetNetworks: [chains.filecoinCalibration],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
