2fa48179a48531d615ef83f8baa2eb07fdc0c3d9
MattWong-ca <matthew.yt.wong@gmail.com>
2024-10-27 22:41:59 +0800

    WIP

--- packages/nextjs/app/axelar/page.tsx
+++ packages/nextjs/app/axelar/page.tsx
@@ -1,9 +1,24 @@
 "use client";
 
 import React, { useState } from "react";
+// import { ToastContainer, toast } from "react-toastify";
 
 const AxelarPage: React.FC = () => {
     const [message, setMessage] = useState("");
+
+    const handleSendMessage = () => {
+        // write(); // Initiating the contract call
+    console.log('asdf')
+        // toast.info("Sending message...", {
+        //   position: "top-right",
+        //   autoClose: 5000,
+        //   hideProgressBar: false,
+        //   closeOnClick: true,
+        //   pauseOnHover: false,
+        //   draggable: true,
+        // });
+      };
+
     return (
         <main className="flex-grow flex flex-col items-center justify-center">
             <h1 className="text-4xl font-bold mb-8 text-center">
@@ -13,7 +28,7 @@ const AxelarPage: React.FC = () => {
             <p className=" mb-8 text-center max-w-3xl text-gray-500">
                 An interchain decentralized application using React, Solidity, and
                 Axelar General Message Passing that allows users to send messages
-                between two chains. This is built off of{" "}
+                between FIL and ETH. This is built off of{" "}
                 <a
                     href="https://github.com/axelarnetwork/fullstack-interchain-dapp-example"
                     target="_blank"
@@ -36,7 +51,7 @@ const AxelarPage: React.FC = () => {
                     />
                     <button
                         className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-6 rounded-full"
-                    // onClick={() => handleSendMessage()}
+                    onClick={() => handleSendMessage()}
                     >
                         Send
                     </button>
