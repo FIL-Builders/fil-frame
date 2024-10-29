fd7ca5a1ef41676a356a96cbe23a9842f2a01ce6
MattWong-ca <matthew.yt.wong@gmail.com>
2024-10-29 11:11:53 +0800

    continue FE

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -19,10 +19,10 @@ export const menuLinks: HeaderMenuLink[] = [
     label: "Home",
     href: "/",
   },
-  // {
-  //   label: "Axelar",
-  //   href: "axelar",
-  // },
+  {
+    label: "Axelar",
+    href: "axelar",
+  },
   {
     label: "Debug Contracts",
     href: "/debug",
