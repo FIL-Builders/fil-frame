9459033cc59e2f4b9a5607b4fd9d88ca19010550
MattWong-ca <matthew.yt.wong@gmail.com>
2024-12-15 21:57:52 -0500

    Mint NFT

--- packages/hardhat/contracts/PythContract.sol
+++ packages/hardhat/contracts/PythContract.sol
@@ -2,24 +2,37 @@
 pragma solidity ^0.8.23;
  
 import "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
  
-contract PythContract {
-  IPyth pyth = IPyth(0xA2aa501b19aff244D90cc15a4Cf739D2725B5729);
+contract PythContract is ERC721URIStorage, Ownable {
+  uint256 private mintedTicketCount;
+  IPyth pyth = IPyth(0xDd24F84d36BF92C65F92307595335bdFab5Bbd21);
   bytes32 filUsdPriceId = 0x150ac9b959aee0051e4091f0ef5216d941f590e1c5e7f91cf7635b5c11628c0e;
  
+  constructor() ERC721("FIL-B NFT", "FILB") Ownable(msg.sender) {}
+
   function mint() public payable {
     PythStructs.Price memory price = pyth.getPriceNoOlderThan(
       filUsdPriceId,
       60
     );
  
-    uint ethPrice18Decimals = (uint(uint64(price.price)) * (10 ** 18)) /
+    uint filPrice18Decimals = (uint(uint64(price.price)) * (10 ** 18)) /
       (10 ** uint8(uint32(-1 * price.expo)));
-    uint oneDollarInWei = ((10 ** 18) * (10 ** 18)) / ethPrice18Decimals;
+    uint oneDollarInAttoFil = ((10 ** 18) * (10 ** 18)) / filPrice18Decimals;
  
-    if (msg.value >= oneDollarInWei) {
-      // User paid enough money.
-      // TODO: mint the NFT here
+    if (msg.value >= oneDollarInAttoFil) {
+      uint256 newTicketId = mintedTicketCount;
+
+      _safeMint(msg.sender, newTicketId);
+
+      _setTokenURI(
+            newTicketId,
+            "ipfs://bafkreicj46qq62emsjp77c5u4iw2fwgmbrvd7vaig4jso4qg56mz3y4hfm"
+        );
+
+      mintedTicketCount++;
     } else {
       revert InsufficientFee();
     }
--- packages/nextjs/app/pyth/page.tsx
+++ packages/nextjs/app/pyth/page.tsx
@@ -1,34 +1,76 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect } from "react";
+import deployedContracts from "../../contracts/deployedContracts";
+import { EvmPriceServiceConnection } from "@pythnetwork/pyth-evm-js";
 import { NextPage } from "next";
+import { parseEther } from "viem";
+import { useAccount } from "wagmi";
+import { useWalletClient, useWriteContract } from "wagmi";
 
 const Home: NextPage = () => {
-  const [price, setPrice] = useState(0);
-  const fetchPythData = async () => {
+  const { address: connectedAddress } = useAccount();
+  const { data: walletClient } = useWalletClient();
+
+  const { writeContract, data: hash, isError, error } = useWriteContract();
+
+  const handleUpdateAndMint = async () => {
     try {
+      if (!walletClient || !connectedAddress) {
+        console.error("Wallet not connected");
+        return;
+      }
+
+      const connection = new EvmPriceServiceConnection("https://hermes.pyth.network");
+      const priceIds = ["0x150ac9b959aee0051e4091f0ef5216d941f590e1c5e7f91cf7635b5c11628c0e"];
+      const priceFeedUpdateData = await connection.getPriceFeedsUpdateData(priceIds);
+      console.log("Retrieved Pyth price update:", priceFeedUpdateData);
+
       const response = await fetch(
         "https://hermes.pyth.network/v2/updates/price/latest?ids[]=0x150ac9b959aee0051e4091f0ef5216d941f590e1c5e7f91cf7635b5c11628c0e",
       );
       const data = await response.json();
-      setPrice(data.parsed[0].price.price * 10 ** data.parsed[0].price.expo);
-      console.log("Pyth Price Data:", data.parsed[0].price);
+      console.log("Pyth Price Data:", data.parsed[0].price.price * 10 ** data.parsed[0].price.expo);
+      console.log("Submitting transaction...");
+
+      const oneDollarInFil = 1 / (data.parsed[0].price.price * 10 ** data.parsed[0].price.expo);
+
+      // If we do exactly one dollar, we may get an insufficient fee error
+      const price = String(oneDollarInFil + oneDollarInFil * 0.01);
+
+      await writeContract({
+        address: deployedContracts[11155111].PythContract.address,
+        abi: deployedContracts[11155111].PythContract.abi,
+        functionName: "updateAndMint",
+        args: [priceFeedUpdateData as any],
+        value: parseEther(price),
+      });
     } catch (error) {
-      console.error("Error fetching Pyth data:", error);
+      console.error("Error in handleUpdateAndMint:", error);
     }
   };
+
+  useEffect(() => {
+    if (isError) {
+      console.error("Transaction failed:", error);
+    }
+    if (hash) {
+      console.log("Transaction submitted! Hash:", hash);
+      const etherscanUrl = `https://sepolia.etherscan.io/tx/${hash}`;
+      console.log("View on Etherscan:", etherscanUrl);
+    }
+  }, [isError, error, hash]);
 
   return (
     <>
       <div>
         <h1>Pyth</h1>
         <button
-          onClick={fetchPythData}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleUpdateAndMint}
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
         >
-          Fetch Pyth Prices
+          Mint
         </button>
-        <p>Price: {price}</p>
       </div>
     </>
   );
