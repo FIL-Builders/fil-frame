b0bfea863bcd5c97c98b871c9bb6771a80775f07
Harsh Singh <harshsingh16d@gmail.com>
2025-02-16 22:18:02 +0530

    fix: hardhat eslint error fixed

--- packages/hardhat/deploy/02_deploy_SrcChain.ts
+++ packages/hardhat/deploy/02_deploy_SrcChain.ts
@@ -15,21 +15,21 @@ const deployContractsOnSrcChain: DeployFunction = async function (
   const { deployer } = await hre.getNamedAccounts();
   console.log("Deploying with account:", deployer);
 
-  let axelarGatewayAddress;
-  let axelarGasReceiver;
-  const network = hre.network.name ;
-  if (network === "calibration") {
-    axelarGatewayAddress = "0x999117D44220F33e0441fbAb2A5aDB8FF485c54D";
-    axelarGasReceiver = "0xbe406f0189a0b4cf3a05c286473d23791dd44cc6";
-  } else if (network === "linea") {
-    axelarGatewayAddress = "0xe432150cce91c13a887f7D836923d5597adD8E31";
-    axelarGasReceiver = "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6";
-  } else {
-    console.error(
-      "Unsupported network. Use 'calibration' for Filecoin testnet, 'linea' for Linear."
-    );
-    process.exit(1);
-  }
+  // let axelarGatewayAddress;
+  // let axelarGasReceiver;
+  // const network = hre.network.name ;
+  // if (network === "calibration") {
+  //   axelarGatewayAddress = "0x999117D44220F33e0441fbAb2A5aDB8FF485c54D";
+  //   axelarGasReceiver = "0xbe406f0189a0b4cf3a05c286473d23791dd44cc6";
+  // } else if (network === "linea") {
+  //   axelarGatewayAddress = "0xe432150cce91c13a887f7D836923d5597adD8E31";
+  //   axelarGasReceiver = "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6";
+  // } else {
+  //   console.error(
+  //     "Unsupported network. Use 'calibration' for Filecoin testnet, 'linea' for Linear."
+  //   );
+  //   process.exit(1);
+  // }
 
   const onramp =  await deploy("OnRampContract", {
     from: deployer,
